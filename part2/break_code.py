2fbf53f2e13757c40f81d7c5add9321fd69e25b4
Neelan Scheumann <ntscheumann@gmail.com>
2020-11-24 00:25:04 -0500

    create prob dicts and mle calc

--- part2/break_code.py
+++ part2/break_code.py
@@ -14,10 +14,25 @@ import math
 import copy 
 import sys
 import encode
+from dictionaries import initial_dict, trans_dict
 
 # put your code here!
+
 def break_code(string, corpus):
     return "Not implemented yet!"
+
+def prob(string, initial_dict, trans_dict):
+    doc_prob = 0
+    word_list = string.split()
+    for word in word_list:
+        word_prob = 0
+        for i in range(len(word)):
+            if i == 0:
+                word_prob += math.log(initial_dict[word[i]])
+            else:
+                word_prob += math.log(trans_dict[word[i-1]][word[i]])
+        doc_prob += word_prob
+    return doc_prob
 
 
 if __name__== "__main__":
